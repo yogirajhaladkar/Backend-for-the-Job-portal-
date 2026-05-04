e42a7f936d8fb746a74f987cece3f76861bb9809
yogiraj2224 <yogiraj.haladkar40@gmail.com>
2026-05-05 02:01:04 +0530

    Connection of the frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,41 @@
 import dotenv from 'dotenv';
-dotenv.config();
 import express from "express";
+
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("hello")
-});
 
-// const port = process.env.PORT;
-// app.listen(port, () => {
-//     console.log(`port $`);
-// });
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: "First Joke",
+            content: "Why did the scarecrow win an award? Because he was outstanding in his field!"
+        },
+        {
+            id: 2,
+            title: "Second Joke",
+            content: "Why don't scientists trust atoms? Because they make up everything!"
+        },
+        {
+            id: 3,
+            title: "Third Joke",
+            content: "What do you call a fake noodle? An impasta!"
+        },
+        {
+            id: 4,
+            title: "Fourth Joke",
+            content: "Why did the bicycle fall over? Because it was two tired!"
+        },
+        {
+            id: 5,
+            title: "Fifth Joke",
+            content: "What do you call a bear with no teeth? A gummy bear!"
+        }
+    ];
+    res.json(jokes);
+});
 
 const PORT = process.env.PORT;
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes, setJokes] = useState([])
 
+  useEffect(() => {
+    axios.get("/api/jokes")
+      .then((response) => {
+        setJokes(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+  }, [])
   return (
     <>
       <h1>heloo </h1>
+      <p>Jokes : {jokes.length}</p>
+
+
+      {
+        jokes.map((joke, index) => (
+          <div key={jokes.id}>
+            <h3>{joke.id}</h3>
+            <h4>{joke.title}</h4>
+            <p>{joke.content}</p>
+          </div>
+        ))
+      }
+
+
     </>
   )
 }
